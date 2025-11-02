4edd3cff48dcb652f646275e929dcc353c55d698
vemulapavan257 <227r5a0506@cmrtc.ac.in>
2025-11-02 12:23:43 +0530

    new files

--- Program to print Swastika Pattern.py
+++ Program to print Swastika Pattern.py
@@ -23,17 +23,19 @@ Output :
 * * * * * *         * '''
 
 
-n=7
+n=15
+m=n//2
 for i in range (n):
-    for j in range (n):
-        if i==(n//2):
-            print('1',end='')
-        elif j==(n//2):
-            print('2',end='')
-        elif j==0 and i<= n//2:
-            print('3',end='')
-        elif i>= n//2 and j==n:
-            print('4',end='')
-        else :
-            print(" ",end ="")
-    print()
+  for j in range (n):
+    if i==m or j==m:
+      print('*',end='')
+    elif i==0 and j>=m:
+      print('*',end='')
+    elif j==0 and i<=m:
+      print('*',end='')
+    elif i==n-1 and j<=m:
+      print('*',end='') 
+    elif j==n-1 and i>=m:
+      print('*',end='')              
+    else: print(' ',end='')
+  print()
